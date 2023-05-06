58456467511a7cdba0a9d198fd724bd8e4cc16cd
fastbeetech <hezekiahfasanya@gmail.com>
2023-05-06 06:58:14 +0100

    refactor product page

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,12 +1,10 @@
 import styles from './Products.module.scss';
 import Featured from './component/Featured';
-import Hero from './component/Hero';
 import Subscribe from './component/Subscribe'
 
 const Products = () => {
   return (
     <main className={styles.container}>
-      <Hero />
       <Featured />
       <Subscribe />
     </main>
--- src/pages/Products/component/Featured.tsx
+++ src/pages/Products/component/Featured.tsx
@@ -7,42 +7,103 @@ import { FaShoppingCart } from 'react-icons/fa';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import '@fontsource/roboto/500.css';
-import { customStyles } from './styles';
+import { productType } from '../../../types';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { SORT, sortProduct } from '../../Categories/request';
 
-const sorts = ['A-Z', 'Z-A', 'Lowest Price', 'Highest Price'];
+type SortOption = keyof typeof SORT;
+const sorts: SortOption[] = ['A-Z', 'Z-A', 'Lowest Price', 'Highest Price'];
 
 const Featured = () => {
-  const { increaseCartQuantity, products } = useCart();
+  const { increaseCartQuantity, products: data } = useCart();
+  const [text, setText] = useState('');
+  const [products, setProducts] = useState(data as productType[]);
+  const [suggestions, setSuggestions] = useState<productType[]>([]);
+  const [isSorted, setIsSorted] = useState(SORT['A-Z']);
 
   const addCartHandler = (product: { id: string }) => {
     toast.info('Item added to cart');
     increaseCartQuantity(product.id);
   };
 
+  useEffect(() => {
+    const sortedProduct = sortProduct(data);
+    setProducts(() => sortedProduct);
+    setIsSorted(isSorted);
+  }, [data, isSorted]);
+
+  const handleSort = (option: SortOption) => {
+    console.log(option);
+    const data = sortProduct(products, SORT[option]);
+    // console.log(data);
+    setIsSorted(SORT[option]);
+    setProducts(() => data);
+  };
+
+  const handleInputChange = (input: string) => {
+    console.log(input);
+    let matches: productType[];
+    if (input.length > 0) {
+      matches = products.filter((product) => {
+        const regex = new RegExp(`${input}`, 'gi');
+        return product.title.match(regex);
+      });
+      setSuggestions(matches);
+      console.log(matches);
+    }
+    setText(input);
+  };
+
   return (
     <div className={styles.featured}>
-      <div style={customStyles.search_wrapper}>
-        <TextField
-          id="outlined-basic"
-          sx={customStyles.searchStyle}
-          type="search"
-          placeholder="search product"
-          variant="outlined"
-        />
+      <div className={styles.search_wrapper}>
+        <div className={styles.searchBox}>
+          <TextField
+            id="outlined-basic"
+            className={styles.searchStyle}
+            type="search"
+            placeholder="search product"
+            variant="outlined"
+            value={text}
+            onChange={({ target }) => handleInputChange(target.value)}
+          />
+          {text.length > 0 && suggestions.length > 0 && (
+            <div className={styles.suggestion}>
+              {suggestions.map((suggestion) => (
+                <Link
+                  className={styles.link}
+                  to={`/product/${suggestion.id}`}
+                  key={suggestion?.id}
+                >
+                  {suggestion?.title}
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
         <TextField
           id="sort"
           select
           label="Sort"
           defaultValue="A-Z"
           variant="filled"
+          className={styles.sortStyle}
         >
           {sorts.map((option) => (
-            <MenuItem key={option} value={option}>
+            <MenuItem
+              key={option}
+              value={option}
+              onClick={() => handleSort(option)}
+            >
               {option}
             </MenuItem>
           ))}
         </TextField>
       </div>
+      <section style={{ margin: '2rem' }}>
+        <h2>All Products</h2>
+      </section>
       <section className={styles.product}>
         {products && products.length > 0 ? (
           products.map((product) => (
